52c53d27a39ec2b8d33330507d861ce4250392e4
squidlywizard <ztdenna@hotmail.com>
2019-01-10 07:53:31 -0700

    Initial Commit

---
+++ recycle project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  recycle project
+//
+//  Created by Denna, Zachary on 1/10/19.
+//  Copyright © 2019 Denna, Zachary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
