b6f8248cbe8d1f0920f62cb3416ed2f49111df92
squidlywizard <ztdenna@hotmail.com>
2019-01-10 09:00:42 -0700

    adde function that allows you to look places up

--- recycle project/ViewController.swift
+++ recycle project/ViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var mapView: MKMapView!
+    
+    @IBOutlet weak var impactButton: UIImageView!
+    @IBOutlet weak var animateButton: UIButton!
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func animateClick(_ sender: UIButton)
+    {
+        let impactLocation = CLLocationCoordinate2D(latitude: -28.6657, longitude: 30.5296)
+        let mapSpan = MKCoordinateSpan(latitudeDelta: 10.05, longitudeDelta: 10.05)
+        let impactRegion = MKCoordinateRegion(center: impactLocation, span: mapSpan)
+        mapView.animatedZoom( zoomRegion: impactRegion, duration: 2.5)
+        
+    }
+    
+    
+    
 
+}
+extension MKMapView
+{
+    public func animatedZoom(zoomRegion:MKCoordinateRegion, duration:TimeInterval)
+    {
+        MKMapView.animate(withDuration: duration, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 10, options: UIView.AnimationOptions.curveEaseIn, animations:
+            {self.setRegion(zoomRegion, animated: true)}, completion: nil)
+    }
 }
 
